e3ce66fb1b769ac1d90c2f0ed541616cb3cd7ef3
aggreyrc <aggreyronoh@gmail.com>
2024-07-21 18:39:54 +0300

    simple liker

--- main.js
+++ main.js
@@ -3,6 +3,41 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.getElementById('modal').classList.add('hidden');
+
+// Add click event listener to the heart
+const hearts = document.querySelectorAll('.like-glyph');
+hearts.forEach(heart => {
+  heart.addEventListener('click', () => {
+    // If the heart is empty, mimic a server call
+    if (heart.innerText === EMPTY_HEART) {
+      mimicServerCall()
+        .then(() => {
+          // Change the heart to a full heart
+          heart.innerText = FULL_HEART;
+          // Add the .activated-heart class to make the heart appear red
+          heart.classList.add('activated-heart');
+        })
+        .catch((error) => {
+          // Display the error modal by removing the .hidden class
+          const errorModal = document.getElementById('modal');
+          errorModal.classList.remove('hidden');
+          // Display the server error message in the modal
+          document.getElementById('modal-message').innerText = error;
+          // Use setTimeout to hide the modal after 3 seconds (add the .hidden class)
+          setTimeout(() => {
+            errorModal.classList.add('hidden');
+          }, 3000);
+        });
+    } else {
+      // If the heart is full, change it back to an empty heart
+      heart.innerText = EMPTY_HEART;
+      // Remove the .activated-heart class
+      heart.classList.remove('activated-heart');
+    }
+  });
+});
+
 
 
 
